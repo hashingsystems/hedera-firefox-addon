4536b8b3057bdd124171cf39abb9de9d7d8af46c
Calvin Cheng <linchuan.cheng@gmail.com>
2019-05-01 16:29:59 +0800

    Add more micropayment tag validation tests using a test expressjs server

---
+++ src/javascript/hedera/tags/micropaymenttagvalidation.test.js
@@ -0,0 +1,44 @@
+import micropaymenttag from './micropaymenttagvalidation'
+
+import express from 'express'
+import path from 'path'
+import fs from 'fs'
+import supertest from 'supertest'
+
+// using express to render our html for tests
+const app = express()
+const port = 9999
+const htmlFileDirectory = path.join(__dirname, '..', 'testdata')
+let routes = []
+
+let server
+let request
+
+beforeEach(done => {
+    const files = fs.readdirSync(htmlFileDirectory)
+    files.forEach(file => {
+        const pathName = file.substring(0, file.length - 5)
+        routes.push(pathName)
+    })
+    const handler = htmlFilePath => {
+        const routeHandler = (req, res) => {
+            res.sendFile(htmlFilePath)
+        }
+        return routeHandler
+    }
+    for (let i = 0; i < routes.length; i++) {
+        app.get(routes[i], handler(files[i]))
+    }
+    server = app.listen(port, done)
+    request = supertest.agent(server)
+})
+
+afterEach(done => {
+    server.close(done)
+})
+
+test('Verify that we can reach all the routes defined for our test express app', () => {
+    routes.forEach(route => {
+        request.get(route).expect(200)
+    })
+})
