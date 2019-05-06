762737d5cd3de8d6acb548ea5f663d2899b123f2
Serene Lim <enerestar@gmail.com>
2019-05-06 11:59:49 +0800

    Make sure it only appears when conditions are met

--- src/javascript/content/index.js
+++ src/javascript/content/index.js
@@ -18,18 +18,20 @@ async function contentListener(msg, sender, sendResponse) {
 
     if (msg.type === 'login') {
         let currentUrl = new URL(window.location.href)
-        let urlRedirect = currentUrl.origin + micropaymentTag.redirect.noAccount
         // if there is a micropayment tag on this web page and
         // the user is not already logged in (i.e. accountID being undefined),
         // we will trigger the alertBanner
         if (micropaymentTag !== null && msg.accountID === undefined) {
+            let urlRedirect = currentUrl.origin + micropaymentTag.redirect.noAccount
             let alertString =
                 "Hang tight! We're redirecting you to the steps on setting up your Hedera Browser Extension."
+
             await alertBanner(alertString, false, false)
             setTimeout(() => {
                 // msg.redirect defaults to /no-account
                 window.location.href = urlRedirect
             }, 6000)
+
         }
     }
 
